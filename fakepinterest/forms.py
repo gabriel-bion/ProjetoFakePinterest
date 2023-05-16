49e5240c3bf703cf5e1d68f40c8061204a407e32
Gabriel Bion <98788606+gabriel-bion@users.noreply.github.com>
2023-05-16 01:45:11 -0300

    ADD Banco de Dados
    adicionando o Banco de Dados da aplicação

---
+++ fakepinterest/forms.py
@@ -0,0 +1,1 @@
+#criar os formularios do site
---
+++ fakepinterest/routes.py
@@ -0,0 +1,12 @@
+#criar as rotas do site(links)
+from flask import render_template, url_for
+from fakepinterest import app
+
+
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+@app.route("/perfil/<usuario>")
+def perfil(usuario):
+    return render_template("perfil.html", usuario=usuario)
